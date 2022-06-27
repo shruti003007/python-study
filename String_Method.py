814acc19c3163f6da6bf6bb46da04f657a6a12d0
shruti003007 <shruti003007@gmail.com>
2022-06-27 20:01:55 +0530

    simple string program

---
+++ String_Method.py
@@ -0,0 +1,8 @@
+Course = "Python Programming"
+print(Course.upper())  # do not effect the original data
+print(Course.lower())
+print(Course.title())  # change 1st alphabet to upper case
+print(Course.find("pro"))
+print(Course.find("Pro"))
+print("programming" in Course)
+print("programming"not in Course)
---
+++ numbers.py
@@ -0,0 +1,11 @@
+x = 10
+x = 0b10
+print(x)
+print(bin(x))
+
+x = 0x12c
+print(hex(x))
+
+# a+bi
+x = 1+2j
+print(x)
