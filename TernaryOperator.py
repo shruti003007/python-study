8d6e13366af0da43d8dfb680c9d7475dd3ffd8e6
shruti003007 <shruti003007@gmail.com>
2022-06-28 18:11:21 +0530

    single line condition

---
+++ TernaryOperator.py
@@ -0,0 +1,10 @@
+age = 22
+#if age >= 18:
+#    message = "eligible"
+#   print (message)
+#else:
+#   message = "not eligible"
+#  print(message)
+
+message = "eligible" if age >= 18 else "not eligible"
+print(message)
