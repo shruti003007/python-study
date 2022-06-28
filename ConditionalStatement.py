05005cbf7ca26632c7cfc93a2056a6b7af544531
shruti003007 <shruti003007@gmail.com>
2022-06-28 17:55:38 +0530

    if, elif n else

---
+++ ConditionalStatement.py
@@ -0,0 +1,9 @@
+temperature=40
+if temperature>30:
+    print("its warm")
+    print("drink water")
+elif temperature>20:
+    print("its nice")
+else:
+    print("its cold")
+print("done")     #will always print
