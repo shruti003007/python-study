11d6369f0c168094d3e174d61d53285b302f7352
shruti003007 <shruti003007@gmail.com>
2022-06-28 13:58:33 +0530

    added working with no

--- numbers.py
+++ numbers.py
@@ -1,3 +1,4 @@
+import math
 x = 1
 x=1.1
 x=1+2j
@@ -8,3 +9,7 @@ print(10/3)
 print(10//3)
 print(10%3)
 print(10**3)
+#working with number
+print(round(2.9))
+print(abs(-2.9))
+print(math.ceil(2.2))
