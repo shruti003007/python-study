3df2a463f7d9061381baaeb69150cb3a1a817da4
shruti003007 <shruti003007@gmail.com>
2022-07-04 12:17:10 +0530

    iterables examples

---
+++ iterables.py
@@ -0,0 +1,12 @@
+print(type (5) )
+print(type (range(5)) )
+
+#iterables
+for x in range(5):  #(range)
+    print(x)
+
+for x in "python": # string
+    print(x)
+
+for x in [1 , 2 , 3 , 4]: #list
+    print(x)
