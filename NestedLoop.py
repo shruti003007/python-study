e91f088725cbf97ef44736b988b0ad5d0c7765bd
shruti003007 <shruti003007@gmail.com>
2022-07-04 11:54:44 +0530

    nestedloop

---
+++ NestedLoop.py
@@ -0,0 +1,11 @@
+for x in range(5):
+    for y in range (3):
+        print(f" ({x}, {y}) ")
+
+color = ["red" , "yellow"]
+num = [1 , 2]
+
+for x in color:
+    for y in num:
+        print(x, y)
+       
