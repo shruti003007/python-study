e4b0ea526a737ff6a733c011f34793588ad1ad86
shruti003007 <shruti003007@gmail.com>
2022-07-04 18:26:07 +0530

    defining functionand arguments

---
+++ Function_n_Arguments.py
@@ -0,0 +1,16 @@
+#defining function
+def greet():
+    print("hi there")
+    print("welcome abroad")
+
+
+greet()
+
+#arguments 
+def greet(first_name, last_name):
+    print(f"hi {first_name}{last_name}")
+    print("welcome aboard")
+
+
+greet("shruti" , "kushwaha")
+#greet("shruti") will display error due to missing 2nd argument
