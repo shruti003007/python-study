4cf6048ee633818d0102640e7c95d26bf3382c30
shruti003007 <shruti003007@gmail.com>
2022-07-07 17:49:08 +0530

    basic function, arguments, type of arguments

--- Function_n_Arguments.py
+++ Function_n_Arguments.py
@@ -1,10 +1,15 @@
 #defining function
+from ast import increment_lineno
+
+
 def greet():
     print("hi there")
     print("welcome abroad")
 
 
 greet()
+
+print("!!!!!!!!!!!!!!!!!!!!!")
 
 #arguments 
 def greet(first_name, last_name):
@@ -14,3 +19,58 @@ def greet(first_name, last_name):
 
 greet("shruti" , "kushwaha")
 #greet("shruti") will display error due to missing 2nd argument
+
+def greet (name):
+    print(f"hi {name}")
+
+
+greet("mosh")
+print(greet("mosh"))  #o/p=>hi mosh   none in which none is the return value of greet function
+
+print("@@@@@@@@@@@@@@@@@@@@@")
+
+#keyword argument
+def increase(number, by):
+    return number + by
+
+result = increase(2,1)
+print(result)
+
+def increase(number, by):
+    return number + by
+
+print(increase(2,1))
+print(increase(2, by = 1))  #by = 1 is a keyword argument
+
+print("############")
+
+#default arguments
+def increament(number, by = 1):
+    return number + by
+
+print(increament(2))  #by default it will take 1 coz of missing 1 argument
+print(increament(2,5)) #both arguments are giver so will print 7
+
+print("%%%%%%%%%%%%%%%%")
+
+#xargs
+def multiply(*numbers):
+    total = 1
+    for number in numbers:
+       # total = total * number
+        total *= number
+    return total
+print(multiply(2,3,4,5))
+
+#basics in xargs
+def multiply(x,y):
+    return x * y
+
+print(multiply(2,3))
+#multiply(2,3,4,5) will not work 
+
+def multiply (*numbers):
+    for number in numbers:
+        print(number)
+
+print(multiply(2,3,4,5))
